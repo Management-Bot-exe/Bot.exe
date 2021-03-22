1cd62407b35c036e6ce19e58d8a03934c42679ad
Commander R <56837737+commander-r@users.noreply.github.com>
2021-03-22 16:44:16 +0100

    Help and verifymessage are an embed now

--- commands/Moderation/verifymessage.js
+++ commands/Moderation/verifymessage.js
@@ -1,21 +1,23 @@
 const { prefix } = require('../../config.json');
-const Discord = require(`discord.js`)
+const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-	name: 'verifymessage',
+	name: 'sendverificationmessage',
 	description: 'List all of my commands or info about a specific command.',
 	aliases: ['svm'],
 	usage: 'svm',
 	cooldown: 5,
-	execute(message, args) {
-    
-        const embed = new Discord.MessageEmbed()
+    execute(message, args, Discord){
+
+    const embed = new MessageEmbed()
         .setTitle(`How To Verify:`)
         .setColor(`#829eef`)
+        .setAuthor(`${message.author.username}`)
+        .setDescription(`When the bot comes public you can add Bot.exe to your server via [this](https://discord.com/oauth2/authorize?client_id=784871149695533076&permissions=8&scope=bot) link!`)
         .addField("How do I verify myself?", `You can verify yourself with doing \`${prefix}verify\` in a channel.`)
         .addField("Why do we need you to verify?", "Because we don't want scam/fake accounts in our server.")
-        .addFooter(`Bot Coded By ***\`Commander R#9371\`***!`)
-        message.channel.send(embed)
-
+        .setFooter(`Bot Coded By Commander R#9371!`)
+        message.channel.send(embed);
     }
 };
--- commands/Utility/help.js
+++ commands/Utility/help.js
@@ -1,46 +1,42 @@
 const { prefix } = require('../../config.json');
+const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	name: 'help',
-	description: 'List all of my commands or info about a specific command.',
-	aliases: ['commands'],
-	usage: '[command name]',
+	description: 'List all of my commands.',
+	aliases: ['h', 'commands'],
+	usage: 'help',
 	cooldown: 5,
-	execute(message, args) {
-		const data = [];
-		const { commands } = message.client;
+    execute(message, args, Discord){
 
-		if (!args.length) {
-			data.push('Here\'s a list of all my commands:');
-			data.push(`\`${commands.map(command => command.name).join('\+ ')}\``);
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-
-			return message.author.send(data, { split: true })
-				.then(() => {
-					if (message.channel.type === 'dm') return;
-					message.reply('I\'ve sent you a DM with all my commands!');
-				})
-				.catch(error => {
-					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-					message.reply('It seems like I can\'t DM you! Please enable DM\'s so i can DM you!');
-				});
-		}
-
-		const name = args[0].toLowerCase();
-		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-		if (!command) {
-			return message.reply('that\'s not a valid command!');
-		}
-
-		data.push(`**Name:** ${command.name}`);
-
-		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-		message.channel.send(data, { split: true });
-	},
+    const embed = new MessageEmbed()
+        .setTitle(`Command List`)
+        .setColor(`#829eef`)
+        .setAuthor(`Commander R#9371`)
+        .setDescription(`These are all the commands the bot can do at the moment!`)
+        .addField("verification", `
+${prefix}verify`)
+		.addField("Utility", `
+${prefix}args-info
+${prefix}avatar
+${prefix}help
+${prefix}invite
+${prefix}serverinfo
+${prefix}support
+${prefix}userinfo
+${prefix}website
+${prefix}ping`)
+		.addField("Moderation", `
+${prefix}ban
+${prefix}kick
+${prefix}purge
+${prefix}svm`)
+		.addField("**Owner Only!**", `
+${prefix}reload
+${prefix}servers
+${prefix}test`)
+        .setFooter(`Bot Coded By Commander R#9371!`)
+        message.channel.send(embed);
+    }
 };
